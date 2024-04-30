f08e4c917b4d229ccc7649a2b041f52a234a5aa9
didichulam <140446190+didichulam@users.noreply.github.com>
2024-04-30 18:46:51 +0100

    added the functionality for the select dropdown

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
 function updateTime() {
   let losAngeles = document.querySelector("#los-angeles");
-  let losAngelesDate = losAngeles.querySelector(".date");
-  let losAngelesTime = losAngeles.querySelector(".time");
-  let losAngelesTimeZone = moment().tz("America/Los_Angeles");
+  if (losAngeles) {
+    let losAngelesDate = losAngeles.querySelector(".date");
+    let losAngelesTime = losAngeles.querySelector(".time");
+    let losAngelesTimeZone = moment().tz("America/Los_Angeles");
 
-  losAngelesDate.innerHTML = losAngelesTimeZone.format("MMMM Do YYYY");
-  losAngelesTime.innerHTML = losAngelesTimeZone.format(
-    "h:mm:ss [<small>]A[</small]"
-  );
+    losAngelesDate.innerHTML = losAngelesTimeZone.format("MMMM Do YYYY");
+    losAngelesTime.innerHTML = losAngelesTimeZone.format(
+      "h:mm:ss [<small>]A[</small]"
+    );
+  }
+
   let paris = document.querySelector("#paris");
-  let parisDate = paris.querySelector(".date");
-  let parisTime = paris.querySelector(".time");
-  let parisTimeZone = moment().tz("Europe/Paris");
+  if (paris) {
+    let parisDate = paris.querySelector(".date");
+    let parisTime = paris.querySelector(".time");
+    let parisTimeZone = moment().tz("Europe/Paris");
 
-  parisDate.innerHTML = parisTimeZone.format("MMMM Do YYYY");
-  parisTime.innerHTML = parisTimeZone.format("h:mm:ss [<small>]A[</small]");
+    parisDate.innerHTML = parisTimeZone.format("MMMM Do YYYY");
+    parisTime.innerHTML = parisTimeZone.format("h:mm:ss [<small>]A[</small]");
+  }
 }
 
+function displayCity(event) {
+  let selectedCityTimeZone = event.target.value;
+  let selectedCityName = selectedCityTimeZone.replace("_", " ").split("/")[1];
+  let selectedCityTime = moment().tz(selectedCityTimeZone);
+  let displayedCities = document.querySelector("#displayed-cities");
+
+  displayedCities.innerHTML = `<div class="city">
+                <div>
+                    <h2>${selectedCityName}</h2>
+                    <div class="date">${selectedCityTime.format(
+                      "MMMM Do YYYY"
+                    )}</div>
+                </div>
+                <div class="time">${selectedCityTime.format(
+                  "h:mm:ss [<small>]A[</small]"
+                )}</div>
+            </div>`;
+}
 updateTime();
 setInterval(updateTime, 1000);
+
+let citySelect = document.querySelector("#city-select");
+citySelect.addEventListener("change", displayCity);
